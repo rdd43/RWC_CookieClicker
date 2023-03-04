f4abe1d5467ac90df34b0a846cb8185c391bb119
Robert Diasio <rdd43@njit.edu>
2023-03-04 12:56:07 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CookieClicker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btn_cookie_click = New System.Windows.Forms.Button()
+        Me.Cookie_Value = New System.Windows.Forms.Label()
+        Me.PassiveCookieTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.btn_click_upgrade = New System.Windows.Forms.Button()
+        Me.btn_passive_one_upgrade = New System.Windows.Forms.Button()
+        Me.lbl_Click_Level_Value = New System.Windows.Forms.Label()
+        Me.lbl_Passive_One_Level = New System.Windows.Forms.Label()
+        Me.lbl_Click_Upgrade_Cost = New System.Windows.Forms.Label()
+        Me.lbl_Passive_One_Upgrade_Cost = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_cookie_click
+        '
+        Me.btn_cookie_click.Location = New System.Drawing.Point(346, 353)
+        Me.btn_cookie_click.Name = "btn_cookie_click"
+        Me.btn_cookie_click.Size = New System.Drawing.Size(321, 105)
+        Me.btn_cookie_click.TabIndex = 0
+        Me.btn_cookie_click.Text = "Click to increase cookies by 1"
+        Me.btn_cookie_click.UseVisualStyleBackColor = True
+        '
+        'Cookie_Value
+        '
+        Me.Cookie_Value.AutoSize = True
+        Me.Cookie_Value.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Cookie_Value.Location = New System.Drawing.Point(363, 9)
+        Me.Cookie_Value.Name = "Cookie_Value"
+        Me.Cookie_Value.Size = New System.Drawing.Size(239, 62)
+        Me.Cookie_Value.TabIndex = 1
+        Me.Cookie_Value.Text = "Cookies: 0"
+        Me.Cookie_Value.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PassiveCookieTimer
+        '
+        Me.PassiveCookieTimer.Interval = 1000
+        '
+        'btn_click_upgrade
+        '
+        Me.btn_click_upgrade.Location = New System.Drawing.Point(12, 58)
+        Me.btn_click_upgrade.Name = "btn_click_upgrade"
+        Me.btn_click_upgrade.Size = New System.Drawing.Size(113, 73)
+        Me.btn_click_upgrade.TabIndex = 2
+        Me.btn_click_upgrade.Text = "Increase Value Of Click"
+        Me.btn_click_upgrade.UseVisualStyleBackColor = True
+        '
+        'btn_passive_one_upgrade
+        '
+        Me.btn_passive_one_upgrade.Location = New System.Drawing.Point(12, 243)
+        Me.btn_passive_one_upgrade.Name = "btn_passive_one_upgrade"
+        Me.btn_passive_one_upgrade.Size = New System.Drawing.Size(113, 73)
+        Me.btn_passive_one_upgrade.TabIndex = 3
+        Me.btn_passive_one_upgrade.Text = "Passive One Upgrade"
+        Me.btn_passive_one_upgrade.UseVisualStyleBackColor = True
+        '
+        'lbl_Click_Level_Value
+        '
+        Me.lbl_Click_Level_Value.AutoSize = True
+        Me.lbl_Click_Level_Value.Font = New System.Drawing.Font("Segoe UI", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_Click_Level_Value.Location = New System.Drawing.Point(12, 134)
+        Me.lbl_Click_Level_Value.Name = "lbl_Click_Level_Value"
+        Me.lbl_Click_Level_Value.Size = New System.Drawing.Size(100, 45)
+        Me.lbl_Click_Level_Value.TabIndex = 4
+        Me.lbl_Click_Level_Value.Text = "LvL: 0"
+        Me.lbl_Click_Level_Value.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_Passive_One_Level
+        '
+        Me.lbl_Passive_One_Level.AutoSize = True
+        Me.lbl_Passive_One_Level.Font = New System.Drawing.Font("Segoe UI", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_Passive_One_Level.Location = New System.Drawing.Point(12, 319)
+        Me.lbl_Passive_One_Level.Name = "lbl_Passive_One_Level"
+        Me.lbl_Passive_One_Level.Size = New System.Drawing.Size(100, 45)
+        Me.lbl_Passive_One_Level.TabIndex = 5
+        Me.lbl_Passive_One_Level.Text = "LvL: 0"
+        Me.lbl_Passive_One_Level.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_Click_Upgrade_Cost
+        '
+        Me.lbl_Click_Upgrade_Cost.AutoSize = True
+        Me.lbl_Click_Upgrade_Cost.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_Click_Upgrade_Cost.Location = New System.Drawing.Point(12, 27)
+        Me.lbl_Click_Upgrade_Cost.Name = "lbl_Click_Upgrade_Cost"
+        Me.lbl_Click_Upgrade_Cost.Size = New System.Drawing.Size(74, 28)
+        Me.lbl_Click_Upgrade_Cost.TabIndex = 6
+        Me.lbl_Click_Upgrade_Cost.Text = "Price: 2"
+        Me.lbl_Click_Upgrade_Cost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_Passive_One_Upgrade_Cost
+        '
+        Me.lbl_Passive_One_Upgrade_Cost.AutoSize = True
+        Me.lbl_Passive_One_Upgrade_Cost.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_Passive_One_Upgrade_Cost.Location = New System.Drawing.Point(12, 212)
+        Me.lbl_Passive_One_Upgrade_Cost.Name = "lbl_Passive_One_Upgrade_Cost"
+        Me.lbl_Passive_One_Upgrade_Cost.Size = New System.Drawing.Size(85, 28)
+        Me.lbl_Passive_One_Upgrade_Cost.TabIndex = 7
+        Me.lbl_Passive_One_Upgrade_Cost.Text = "Price: 10"
+        Me.lbl_Passive_One_Upgrade_Cost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CookieClicker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(982, 470)
+        Me.Controls.Add(Me.lbl_Passive_One_Upgrade_Cost)
+        Me.Controls.Add(Me.lbl_Click_Upgrade_Cost)
+        Me.Controls.Add(Me.lbl_Passive_One_Level)
+        Me.Controls.Add(Me.lbl_Click_Level_Value)
+        Me.Controls.Add(Me.btn_passive_one_upgrade)
+        Me.Controls.Add(Me.btn_click_upgrade)
+        Me.Controls.Add(Me.Cookie_Value)
+        Me.Controls.Add(Me.btn_cookie_click)
+        Me.Name = "CookieClicker"
+        Me.Text = "Cookie Clicker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_cookie_click As Button
+    Friend WithEvents Cookie_Value As Label
+    Friend WithEvents PassiveCookieTimer As Timer
+    Friend WithEvents btn_click_upgrade As Button
+    Friend WithEvents btn_passive_one_upgrade As Button
+    Friend WithEvents lbl_Click_Level_Value As Label
+    Friend WithEvents lbl_Passive_One_Level As Label
+    Friend WithEvents lbl_Click_Upgrade_Cost As Label
+    Friend WithEvents lbl_Passive_One_Upgrade_Cost As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class CookieClicker
+    'global variable
+    Public totalCookies, ClickValue, ClickLevel As Integer
+    Public PassiveOneVal, PassiveOneLevel As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        totalCookies = 0
+        ClickValue = 1
+        ClickLevel = 1
+
+        PassiveOneVal = 0
+        PassiveOneLevel = 1
+        'comment in visual basic
+        'Private Sub means funciton
+        'Form1_Load just means when you start the program
+
+        'starts the time
+        set_Timer(1000)
+        '1000 is once a second
+        '100 is 1/10 of a second
+        PassiveCookieTimer.Start()
+    End Sub
+
+    Private Sub btn_cookie_click_Click(sender As Object, e As EventArgs) Handles btn_cookie_click.Click
+        cookie_click()
+    End Sub
+
+    'helper function
+    'increments the cookie value
+    Private Sub cookie_click()
+        totalCookies = totalCookies + ClickValue
+        Cookie_Value.Text = "Cookies: " + totalCookies.ToString
+    End Sub
+
+    'Passive Cookie Function
+    Private Sub PassiveCookieTimer_Tick(sender As Object, e As EventArgs) Handles PassiveCookieTimer.Tick
+        totalCookies = totalCookies + PassiveOneVal
+        Cookie_Value.Text = "Cookies: " + totalCookies.ToString
+    End Sub
+
+    'helper function
+    Private Sub set_Timer(interval As Integer)
+        PassiveCookieTimer.Interval = interval
+    End Sub
+
+    Private Sub btn_click_upgrade_Click(sender As Object, e As EventArgs) Handles btn_click_upgrade.Click
+        Dim upgradeCost As Integer
+        upgradeCost = ClickLevel * 2
+
+        If totalCookies >= upgradeCost Then
+            totalCookies = totalCookies - upgradeCost
+
+            ClickLevel = ClickLevel + 1
+            ClickValue = (ClickLevel - 1) * 5
+
+            upgradeCost = ClickLevel * 2
+            lbl_Click_Level_Value.Text = "LvL: " + ClickLevel.ToString
+            lbl_Click_Upgrade_Cost.Text = "Price: " + upgradeCost.ToString
+            btn_cookie_click.Text = "Click to increase cookies by " + ClickValue.ToString
+        End If
+    End Sub
+
+    Private Sub btn_passive_one_upgrade_Click(sender As Object, e As EventArgs) Handles btn_passive_one_upgrade.Click
+        Dim upgradeCost As Integer
+        upgradeCost = PassiveOneLevel * PassiveOneLevel * 10
+        If totalCookies >= upgradeCost Then
+            totalCookies = totalCookies - upgradeCost
+
+            PassiveOneLevel = PassiveOneLevel + 1
+            PassiveOneVal = PassiveOneVal + 10
+            upgradeCost = PassiveOneLevel * PassiveOneLevel * 10
+
+            lbl_Passive_One_Level.Text = "LvL: " + PassiveOneLevel.ToString
+            lbl_Passive_One_Upgrade_Cost.Text = "Price: " + upgradeCost.ToString
+        End If
+    End Sub
+End Class
